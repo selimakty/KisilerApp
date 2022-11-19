f56498dc314adeda3a4fd246b2478531001cde63
Selim Aktay <sellimakty99@gmail.com>
2022-11-19 15:20:25 +0300

    Initial Commit

---
+++ KisilerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerApp
+//
+//  Created by Selim on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
